dce7fc0a1ca66da283ee4931d2fe161a4cf1f44e
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-29 00:23:11 +0900

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var jade = require('jade');
+var gulpJade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var cssnano = require('gulp-cssnano');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('jade', function () {
+  return gulp.src('*.jade')
+    .pipe(gulpJade({
+      jade: jade,
+      pretty: false
+    }))
+    .pipe(gulp.dest('build/'))
+})
+
+gulp.task('stylus', function () {
+  return gulp.src('*.styl')
+    .pipe(stylus())
+    .pipe(cssnano())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('coffee', function() {
+  gulp.src('*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('build/'))
+});

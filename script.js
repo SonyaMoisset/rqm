f79620102be34a52ede2e1b9ec7551725d663ad2
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-29 21:33:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-$(function($) {
-  $('#get-another-quote-button').on('click', function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function(data) {
-        var quote;
-        quote = data.shift();
-        $('#quote-title').text(quote.title);
-        $('#quote-content').html(quote.content);
-        $('#tweet').on('click', function() {
-          $(this).attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent($('#quote-content').text()) + '- ' + $('#quote-title').text());
-        });
-      },
-      cache: false
-    });
-  });
-});
+(function(){$(function(t){t("#get-another-quote-button").on("click",function(e){e.preventDefault(),t.ajax({url:"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",success:function(e){var n;n=e.shift(),t("#quote-title").text(n.title),t("#quote-content").html(n.content),t("#tweet").on("click",function(){t(this).attr("href","https://twitter.com/intent/tweet?text="+encodeURIComponent(t("#quote-content").text())+"- "+t("#quote-title").text())})},cache:!1})})})}).call(this);
